0a168cc7b05dba033fc845de5836bcfc12633e83
FujiiKohaku <k024g0078@g.neec.ac.jp>
2025-10-20 15:02:53 +0900

    a

--- project/main.cpp
+++ project/main.cpp
@@ -11,7 +11,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
     // -------------------------------
     // 1. DirectXリソースリーク検出準備
     // ------------------------------- 
-    D3DResourceLeakChecker leakChecker; // 終了時にリソースリークを自動検出
+    D3DResourceLeakChecker leakChecker; // 終了時にリソースリークを自動検出 
 
     // -------------------------------
     // 2. ゲーム本体の生成と初期化
